2f0a41637c7e16240e11210adf989a3d603306ed
Taketo Yahiro <yahiro.t.eng@gmail.com>
2021-02-07 17:31:55 +0900

    add link and adjust js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,8 +3,14 @@
 {
   const open_nav = document.getElementById('open-nav');
   const close_nav = document.getElementById('close-nav');
+  const nav_items = document.getElementsByClassName('nav-item');
   const nav = document.getElementById('nav');
 
+  for(let i = 0;i<nav_items.length;i++){
+    nav_items[i].addEventListener('click', ()=>{
+      nav.classList.toggle('open');
+    });
+  }
 
   open_nav.addEventListener('click', ()=>{
     nav.classList.toggle('open');
@@ -14,6 +20,4 @@
   close_nav.addEventListener('click', ()=>{
     nav.classList.remove('open');
   });
-
-  
 }
